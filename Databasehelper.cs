1383ce355b793fc0d168d72dd023b7cf3aba8a17
Dinh Khuong <lanphuongtran1999@gmail.com>
2025-04-20 00:10:46 +0700

    khuong

--- Databasehelper.cs
+++ Databasehelper.cs
@@ -12,6 +12,7 @@ namespace FINAL_PROJECT_ST2
         static DatabaseHelper()
         {
             CurrentConnectionString = @"Data Source=KHUONG;Initial Catalog=TEST_DATABASE;Integrated Security=True;Encrypt=False;TrustServerCertificate=True";
+            // Data Source=KHUONG\SQLEXPRESS;Initial Catalog=DATABASESCRIPS;Integrated Security=True;Encrypt=False;TrustServerCertificate=True
         }
 
         // Cấu hình chuỗi kết nối bằng user/password SQL Server
@@ -22,7 +23,7 @@ namespace FINAL_PROJECT_ST2
         public static void SetConnection(string user, string password)
         {
             //CurrentConnectionString = $"Data Source=KHUONG;Initial Catalog=TEST_DATABASE;User ID=hoasales;Password=sales456 ;Encrypt=False;TrustServerCertificate=True
-            CurrentConnectionString = @"Data Source=KHUONG;Initial Catalog=TEST_DATABASE;Integrated Security=True;Encrypt=False;TrustServerCertificate=True";
+            CurrentConnectionString = @"Data Source=KHUONG\SQLEXPRESS;Initial Catalog=DATABASESCRIPS;Integrated Security=True;Encrypt=False;TrustServerCertificate=True";
 
         }
         // Tạo kết nối từ chuỗi tĩnh
